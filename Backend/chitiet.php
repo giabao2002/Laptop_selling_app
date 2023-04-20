60bc618be3aa15a71de1c537e12b440765e7b854
Hoàng Gia Bảo <100865900+giabao2002@users.noreply.github.com>
2023-04-21 00:35:57 +0700

    backend

---
+++ Backend/chitiet.php
@@ -0,0 +1,28 @@
+<?php
+include "connect.php";
+$page = $_POST['page'];
+$total = 5; // can lay 5 san pham tren 1 trang
+$pos = ($page - 1) * $total; // 0,5 5,5
+$loai = $_POST['loai'];
+
+$query = 'SELECT * FROM `sanphammoi` WHERE `loai`= ' . $loai . ' LIMIT ' . $page . ', ' . $total . '
+';
+$data = mysqli_query($conn, $query);
+$result = array();
+while ($row = mysqli_fetch_assoc($data)) {
+    $result[] = ($row);
+}
+if (!empty($result)) {
+    $arr = [
+        'success' => true,
+        'message' => "thanh cong",
+        'result' => $result
+    ];
+} else {
+    $arr = [
+        'success' => false,
+        'message' => "that bai",
+        'result' => $result
+    ];
+}
+print_r(json_encode($arr));
