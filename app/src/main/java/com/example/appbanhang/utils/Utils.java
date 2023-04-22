165bc6e843f31824282d48080b57dc64df4a7c0d
Hoàng Gia Bảo <100865900+giabao2002@users.noreply.github.com>
2023-04-22 14:47:26 +0700

    test new branch

--- app/src/main/java/com/example/appbanhang/utils/Utils.java
+++ app/src/main/java/com/example/appbanhang/utils/Utils.java
@@ -1,5 +1,5 @@
 package com.example.appbanhang.utils;
 
 public class Utils {
-    public static final String BASE_URL = "http://192.168.236.187/banhang/";
+    public static final String BASE_URL = "http://192.168.236.187/banhang/a";
 }
