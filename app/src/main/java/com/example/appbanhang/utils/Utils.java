1122656a1f3ac74873fb59f5d8405d455ca4548b
Hoàng Gia Bảo <100865900+giabao2002@users.noreply.github.com>
2023-04-23 16:19:17 +0700

    delete data

--- app/src/main/java/com/example/appbanhang/utils/Utils.java
+++ app/src/main/java/com/example/appbanhang/utils/Utils.java
@@ -1,5 +1,9 @@
 package com.example.appbanhang.utils;
 
 public class Utils {
-    public static final String BASE_URL = "http://192.168.1.101/banhang/";
+    public static final String BASE_URL = "http://192.168.24.105/banhang/";
 }
+
+//ip của Bảo: 192.168.24.105
+//ip của Khang:
+//ip của Tuấn Anh:
