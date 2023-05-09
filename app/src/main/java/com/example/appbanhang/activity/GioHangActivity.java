fd2d8d9493a1bef898382f00ef38368f9bc0fac7
ngokhang <ngokhang2002vn@gmail.com>
2023-05-10 00:40:59 +0700

    giao diện đặt hàng

--- app/src/main/java/com/example/appbanhang/activity/GioHangActivity.java
+++ app/src/main/java/com/example/appbanhang/activity/GioHangActivity.java
@@ -5,6 +5,7 @@ import androidx.appcompat.widget.Toolbar;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -68,6 +69,14 @@ public class GioHangActivity extends AppCompatActivity {
             adapter = new GioHangAdapter(getApplicationContext(),Utils.manggiohang);
             recyclerView.setAdapter(adapter);
         }
+
+        btnmuahang.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(), ThanhToanAcitivity.class);
+                startActivity(intent);
+            }
+        });
     }
 
     private void initView() {
