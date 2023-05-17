317c7efca1a0397f606b1e047e2a8ee84199a222
Bùi Đức Tuấn Anh <20010838@st.phenikaa-uni.edu.vn>
2023-05-17 12:02:47 +0700

    Xử lí xem đơn hàng

--- app/src/main/java/com/example/appbanhang/activity/XemDonActivity.java
+++ app/src/main/java/com/example/appbanhang/activity/XemDonActivity.java
@@ -1,11 +1,16 @@
 package com.example.appbanhang.activity;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Toast;
 
 import com.example.appbanhang.R;
+import com.example.appbanhang.adapter.DonHangAdapter;
 import com.example.appbanhang.retrofit.ApiBanHang;
 import com.example.appbanhang.retrofit.RetrofitClient;
 import com.example.appbanhang.utils.Utils;
@@ -18,23 +23,62 @@ import io.reactivex.rxjava3.schedulers.Schedulers;
 public class XemDonActivity extends AppCompatActivity {
     CompositeDisposable compositeDisposable = new CompositeDisposable();
     ApiBanHang apiBanHang;
+    RecyclerView redonhang;
+    Toolbar toolbar;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_xem_don);
-        apiBanHang = RetrofitClient.getInstance(Utils.BASE_URL).create(ApiBanHang.class);
+        initView();
+        initToolbar();
+        getOder();
+
+
+
+
+    }
+
+    private void getOder() {
         compositeDisposable.add(apiBanHang.xemDonHang(Utils.user_current.getId())
                 .subscribeOn(Schedulers.io())
                 .observeOn(AndroidSchedulers.mainThread())
                 .subscribe(
                         donHangModel -> {
-                            Toast.makeText(getApplicationContext(), donHangModel.getResult().get(0).getItem().get(1).getTensp(), Toast.LENGTH_SHORT).show();
+                            DonHangAdapter donHangAdapter = new DonHangAdapter(getApplicationContext(),donHangModel.getResult());
+                            redonhang.setAdapter(donHangAdapter);
                         },
                         throwable -> {
 
                         }
                 )
         );
+    }
 
+    private void initToolbar() {
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        toolbar.setNavigationOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
+
+    }
+
+    private void initView() {
+        apiBanHang = RetrofitClient.getInstance(Utils.BASE_URL).create(ApiBanHang.class);
+        redonhang = findViewById(R.id.recyclerview_donhang);
+        toolbar = findViewById(R.id.toobar);
+        LinearLayoutManager layoutManager = new LinearLayoutManager(this);
+        redonhang.setLayoutManager(layoutManager);
+    }
+
+    @Override
+    protected void onDestroy() {
+        compositeDisposable.clear();
+        super.onDestroy();
     }
 }
---
+++ app/src/main/java/com/example/appbanhang/adapter/ChitietAdapter.java
@@ -0,0 +1,63 @@
+package com.example.appbanhang.adapter;
+
+import android.content.Context;
+import android.media.Image;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.bumptech.glide.Glide;
+import com.example.appbanhang.R;
+import com.example.appbanhang.model.Item;
+import com.google.gson.Gson;
+
+import java.util.List;
+
+public class ChitietAdapter extends RecyclerView.Adapter<ChitietAdapter.MyViewHolder> {
+    Context context;
+    List<Item> itemList;
+
+    public ChitietAdapter(Context context, List<Item> itemList) {
+        this.context = context;
+        this.itemList = itemList;
+    }
+
+    @NonNull
+    @Override
+    public MyViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view =LayoutInflater.from(parent.getContext()).inflate(R.layout.item_donhang,parent,false);
+        return new MyViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull MyViewHolder holder, int position) {
+        Item item = itemList.get(position);
+        holder.txtten.setText(item.getTensp()+ "");
+    }
+
+    @Override
+    public int getItemCount() {
+        return itemList.size();
+    }
+
+    public class MyViewHolder extends RecyclerView.ViewHolder{
+        TextView txtten, txtsoluong;
+        ImageView imagechitiet;
+
+        public MyViewHolder(@NonNull View itemView) {
+            super(itemView);
+            imagechitiet=itemView.findViewById(R.id.item_imgchitiet);
+            txtten =itemView.findViewById(R.id.item_tenspchitiet);
+            txtsoluong= itemView.findViewById(R.id.item_soluongchitiet);
+
+        }
+    }
+
+}
