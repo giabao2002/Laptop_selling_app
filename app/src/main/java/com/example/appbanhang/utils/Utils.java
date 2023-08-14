67c6f8ab6aa3a2ca071a2f90e7c5cadf5e50d740
Hoàng Gia Bảo <100865900+giabao2002@users.noreply.github.com>
2023-08-14 20:54:24 +0700

    fix ip

--- app/src/main/java/com/example/appbanhang/utils/Utils.java
+++ app/src/main/java/com/example/appbanhang/utils/Utils.java
@@ -6,11 +6,11 @@ import com.example.appbanhang.model.User;
 import java.util.List;
 
 public class Utils {
-    public static final String BASE_URL = "http://10.105.26.209/banhang/";
+    public static final String BASE_URL = "http://192.168.24.112/appbanhang/";
     public static List<GioHang> manggiohang;
     public static User user_current = new User();
 }
 
-//ip của Bảo: 192.168.24.105
+//ip của Bảo: 192.168.1.20
 //ip của Khang: 192.168.1.101 || 10.190.11.166
 //ip của Tuấn Anh:
