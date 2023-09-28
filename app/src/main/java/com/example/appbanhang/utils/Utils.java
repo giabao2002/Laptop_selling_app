12f984cfd32fdef7c7f2f6fc341e28a19377d458
Hoàng Gia Bảo <100865900+giabao2002@users.noreply.github.com>
2023-09-28 16:40:00 +0700

    update

--- app/src/main/java/com/example/appbanhang/utils/Utils.java
+++ app/src/main/java/com/example/appbanhang/utils/Utils.java
@@ -6,11 +6,10 @@ import com.example.appbanhang.model.User;
 import java.util.List;
 
 public class Utils {
-    public static final String BASE_URL = "http://192.168.24.112/appbanhang/";
+    public static final String BASE_URL = "http://192.168.24.107/appbanhang/";
     public static List<GioHang> manggiohang;
     public static User user_current = new User();
 }
 
-//ip của Bảo: 192.168.1.20
+//ip của Bảo: 192.168.24.107
 //ip của Khang: 192.168.1.101 || 10.190.11.166
-//ip của Tuấn Anh:
